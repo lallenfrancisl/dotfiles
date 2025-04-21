cd0c82277c60803ac4eb56ab2adc7e34fd52054f
Allen Francis <lallenfrancisl@gmail.com>
2025-04-22 01:22:41 +0530

    add script to setup tmux plugin automatically

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+if [ ! -d ~/.config/tmux/plugins/tpm ]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
+else
+    cd ~/.config/tmux/plugins/tpm
+    git checkout master
+    git pull origin master
+fi
+
+stow .

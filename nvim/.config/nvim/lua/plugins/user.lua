2d0aa347496a498a6f899a09576ec3a7f795c8b6
Allen Francis <lallenfrancisl@gmail.com>
2026-07-14 14:28:14 +0530

    Add vue snippets to lua snip suggestions

--- nvim/.config/nvim/lua/plugins/user.lua
+++ nvim/.config/nvim/lua/plugins/user.lua
@@ -2,8 +2,9 @@
 return {
   {
     "L3MON4D3/LuaSnip",
-    opts = {
-      history = false,
-    },
+    opts = function(_, opts)
+      opts.history = false
+      require("luasnip").filetype_extend("vue", { "html" })
+    end,
   },
 }

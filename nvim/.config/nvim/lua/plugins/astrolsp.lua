eac625312adbeb3a5d6537dc807a5ee96788008a
Allen Francis <lallenfrancisl@gmail.com>
2026-07-14 14:27:51 +0530

    Configure oxfmt for typescript and vue formatting

--- nvim/.config/nvim/lua/plugins/astrolsp.lua
+++ nvim/.config/nvim/lua/plugins/astrolsp.lua
@@ -6,22 +6,35 @@ return {
   "AstroNvim/astrolsp",
   ---@type AstroLSPOpts
   opts = {
+    servers = { "oxfmt" },
+    formatting = {
+      filter = function(client)
+        local oxfmt_filetypes = {
+          javascript = true,
+          javascriptreact = true,
+          typescript = true,
+          typescriptreact = true,
+          vue = true,
+        }
+
+        return not oxfmt_filetypes[vim.bo.filetype] or client.name == "oxfmt"
+      end,
+    },
     config = {
+      oxfmt = {
+        cmd = function(dispatchers, config)
+          local cmd = vim.fs.joinpath(config.root_dir, "node_modules/vite-plus/bin/oxfmt")
+          if vim.fn.executable(cmd) ~= 1 then cmd = "oxfmt" end
+
+          return vim.lsp.rpc.start({ cmd, "--lsp" }, dispatchers)
+        end,
+      },
       gopls = {
         settings = {
           gopls = {
             directoryFilters = { "-reference-code" },
           },
         },
-      },
-      vue_ls = {
-        cmd = function(dispatchers, config)
-          return vim.lsp.rpc.start({
-            "vue-language-server",
-            "--stdio",
-            "--tsdk=" .. config.root_dir .. "/node_modules/typescript/lib",
-          }, dispatchers)
-        end,
       },
     },
   },

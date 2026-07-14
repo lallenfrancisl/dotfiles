f465a3057cfa19520c254463c334ea68ebc63e76
Allen Francis <lallenfrancisl@gmail.com>
2026-07-14 15:23:53 +0530

    Add the updated vue configuration instead of astrocommunity pack

--- nvim/.config/nvim/lua/plugins/astrocore.lua
+++ nvim/.config/nvim/lua/plugins/astrocore.lua
@@ -15,6 +15,48 @@ return {
     }
     opts.diagnostics.signs = false
 
+    opts.mappings.n["grr"] = {
+      function()
+        local current_file = vim.api.nvim_buf_get_name(0)
+        local cursor = vim.api.nvim_win_get_cursor(0)
+
+        vim.lsp.buf.references(nil, {
+          on_list = function(list)
+            local references, seen = {}, {}
+            for _, item in ipairs(list.items) do
+              local key = table.concat({ item.filename, item.lnum, item.col, item.end_lnum, item.end_col }, ":")
+              local current = item.filename == current_file
+                and item.lnum == cursor[1]
+                and cursor[2] + 1 >= item.col
+                and cursor[2] + 1 <= (item.end_col or item.col)
+
+              if not current and not seen[key] then
+                seen[key] = true
+                references[#references + 1] = item
+              end
+            end
+
+            if #references == 0 then
+              vim.notify "No other references found"
+            elseif #references == 1 then
+              local reference = references[1]
+              vim.cmd "normal! m'"
+              if reference.filename ~= vim.api.nvim_buf_get_name(0) then
+                vim.cmd.edit(vim.fn.fnameescape(reference.filename))
+              end
+              vim.api.nvim_win_set_cursor(0, { reference.lnum, reference.col - 1 })
+              vim.cmd "normal! zv"
+            else
+              list.items = references
+              vim.fn.setqflist({}, " ", list)
+              vim.cmd "botright copen"
+            end
+          end,
+        })
+      end,
+      desc = "Go to references",
+    }
+
     opts.mappings.n["<Leader>Sl"] = {
       function()
         local ok, _ = pcall(function() require("resession").load(vim.fn.getcwd(), { dir = "dirsession" }) end)
---
+++ nvim/.config/nvim/lua/plugins/vue.lua
@@ -0,0 +1,54 @@
+---@type LazySpec
+return {
+  "AstroNvim/astrolsp",
+  ---@param opts AstroLSPOpts
+  opts = function(_, opts)
+    local astrocore = require "astrocore"
+    local vue_language_server_path = vim.fn.stdpath "data"
+      .. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
+    local typescript_sdk_path = vim.fn.stdpath "data"
+      .. "/mason/packages/vtsls/node_modules/@vtsls/language-server/node_modules/typescript/lib"
+
+    local function project_paths(root_dir)
+      if not root_dir then return typescript_sdk_path, vue_language_server_path, "vue-language-server" end
+
+      local project_typescript_sdk = vim.fs.joinpath(root_dir, "node_modules/typescript/lib")
+      local project_vue_language_server = vim.fs.joinpath(root_dir, "node_modules/@vue/language-server")
+      local project_vue_ls = vim.fs.joinpath(root_dir, "node_modules/.bin/vue-language-server")
+
+      return vim.fn.isdirectory(project_typescript_sdk) == 1 and project_typescript_sdk or typescript_sdk_path,
+        vim.fn.isdirectory(project_vue_language_server) == 1 and project_vue_language_server or vue_language_server_path,
+        vim.fn.executable(project_vue_ls) == 1 and project_vue_ls or "vue-language-server"
+    end
+
+    local function vue_plugin(location)
+      return {
+        name = "@vue/typescript-plugin",
+        location = location,
+        languages = { "vue" },
+        configNamespace = "typescript",
+      }
+    end
+
+    opts.servers = astrocore.list_insert_unique(opts.servers, { "vtsls", "vue_ls" })
+    opts.config.volar = nil
+    opts.config.vue_ls = {
+      cmd = function(dispatchers, config)
+        local tsdk, _, cmd = project_paths(config.root_dir)
+        return vim.lsp.rpc.start({ cmd, "--stdio", "--tsdk=" .. tsdk }, dispatchers)
+      end,
+    }
+    opts.config.vtsls.before_init = function(_, config)
+      local _, vue_ls_path = project_paths(config.root_dir)
+      config.settings.vtsls.tsserver.globalPlugins = { vue_plugin(vue_ls_path) }
+    end
+    opts.config.vtsls.filetypes = {
+      "typescript",
+      "javascript",
+      "javascriptreact",
+      "typescriptreact",
+      "vue",
+    }
+    opts.config.vtsls.settings.vtsls.tsserver.globalPlugins = { vue_plugin(vue_language_server_path) }
+  end,
+}
